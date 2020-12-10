432c128bb6f993bae0e7d49406b8a724674c28e7
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-12-10 10:53:16 +0100

    Create SpotifyClient class

--- src/main.py
+++ src/main.py
@@ -1,18 +1,42 @@
 import os
 import time
+import base64
+import requests
+from datetime import datetime
 from dotenv import load_dotenv
 
-# class SpotifyClient:
-#     def __init__(self):
-#         self.user_id = spotify_user_id
-
-#     def pause(self):
 
 load_dotenv()
 
-MY_ENV_VAR = os.getenv('TEST')
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
-print(MY_ENV_VAR)
+token_url = "https://accounts.spotify.com/api/token"
+token_data = {
+    "grant_type": "client_credentials"
+}
+
+client_credentials = f"{CLIENT_ID}:{CLIENT_SECRET}"
+client_credentials_b64 = base64.b64encode(client_credentials.encode())
+
+token_headers = {
+    "Authorization": f"Basic {client_credentials_b64.decode()}"
+}
+
+response = requests.post(token_url, data=token_data, headers=token_headers)
+print(response.json())
+
+class SpotifyClient:
+    client_id = None
+    client_secret = None
+    access_token = None
+    access_token_expires = datetime.now()
+
+    def __init__(self, client_id, client_secret, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.client_id = client_id
+        self.client_secret = client_secret
+
 
 
 
