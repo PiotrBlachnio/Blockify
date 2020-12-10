f18166a35149f1a25638424115e7b012c75ad15c
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-12-10 09:35:08 +0100

    Create open_application method

--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
 import os
+import time
+
+path = "C:\\Users\\Piotr Błachnio\\Desktop\\Spotify"
 
 def close_application():
     os.system("TASKKILL /F /IM Spotify.exe")
 
+def open_application():
+    os.startfile(path)
+
 if __name__ == "__main__":
     close_application()
+    time.sleep(1)
+    open_application()
